9a2dff29f7f9fddb6dd186b4dd281f3ff6bdd4be
felipe aristizabal <felipe.aristizabal12@outlook.com>
2023-06-27 21:30:10 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>my App</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid excepturi dolor animi ipsam. Quas blanditiis sunt excepturi recusandae? Qui, praesentium accusantium recusandae animi corrupti quia nobis eos unde expedita dolores.</p>
+    </div>
+  );
+}
+
+export default App;
