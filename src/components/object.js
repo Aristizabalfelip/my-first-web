69768ab2e71f458c777014b05de278dbc295c948
Felipe Aristizabal <felipe.aristizabal12@outlook.com>
2023-06-28 00:41:56 +0200

    commit house

---
+++ src/components/object.js
@@ -0,0 +1,33 @@
+const infoCards = [
+    {
+        type: 'visa',
+        number: 1234431267566984,
+        expirationMonth: 8,
+        expirationYear: 27,
+        bank: 'BBVA',
+        owner: 'Francis Sinaloa',
+        cssClass: 'goldCreditCard'
+
+    },
+    {
+        type: 'MasterCard',
+        number: 1294441566560995,
+        expirationMonth: 10,
+        expirationYear: 29,
+        bank: 'N26',
+        owner: 'Julio Sinatra',
+        cssClass: 'silverCreditCard'
+
+    },
+    {
+        type: 'visa',
+        number: 4534463698568845,
+        expirationMonth: 11,
+        expirationYear: 25,
+        bank: 'BNP',
+        owner: 'Cristopher Caceres',
+        cssClass: 'basicCreditCard'
+
+    }
+]
+export default infoCards;
